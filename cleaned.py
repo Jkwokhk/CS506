f891988a915da78698f87b63762379761cf3cd81
HarrisonChe5188 <0979harrison@gmail.com>
2025-03-30 15:42:16 -0400

    focused on Stardew valle, open to change later

--- cleaned.py
+++ cleaned.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv("weighted_score_above_08.csv")
-new_df = df[(df["language"]=="english") & (df["game"]=="Counter-Strike 2")]
+new_df = df[(df["language"]=="english") & (df["game"]=="Stardew Valley")]
 new_df.to_csv('cleaned_data.csv', index=False)
